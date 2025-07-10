a75068023addd65375eb2d194357af684f8beaad
siddheshkadane01 <siddheshkadane@gmail.com>
2025-07-10 11:50:06 +0530

    dep-e1

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,15 +1,14 @@
 import { Webhook } from "svix";
 import User from "../models/User";
-import e from "express";
 
 // API controller function to manage clerk user with database
 export const clerkWebhook = async (req, res) => {
-    try{
+    try {
         //create a svix webhook instance
         const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
         //Verify the headers
-        await webhook.verify(JSON.stringify(req.body),{
+        await webhook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
@@ -19,46 +18,44 @@ export const clerkWebhook = async (req, res) => {
         const { data, type } = req.body;
 
         //switch case to handle different webhook events
-        switch(type){
+        switch(type) {
             case "user.created":
                 //create a new user in the database
                 const userData = {
                     _id: data.id,
                     email: data.emailAddresses[0].emailAddress,
                     name: data.firstName + " " + data.lastName,
-                    image: data.image_url,  // Added comma here
-                    resume:''
+                    image: data.image_url,
+                    resume: ''
                 }
                 await User.create(userData);
-                res.json({})
                 break;
 
             case "user.updated":
                 //update the user in the database
-                const updatedUserData = {  // Changed variable name to avoid redeclaration
+                const updatedUserData = {
                     email: data.emailAddresses[0].emailAddress,
                     name: data.firstName + " " + data.lastName,
                     image: data.image_url
                 }
                 await User.findByIdAndUpdate(data.id, updatedUserData);
-                res.json({})
                 break;
 
             case "user.deleted":
                 //delete the user from the database
                 await User.findByIdAndDelete(data.id);
-                res.json({})
                 break;
 
             default:
                 console.log("Unhandled event type:", type);
         }
 
-    }
-
-
-    catch (error){
-        console.log(error.message);
-        res.json({success: false, message: "Webhooks error"});
+        // Send response only once after switch case completes
+        return res.status(200).json({ success: true });
+    } 
+    catch (error) {
+        console.error("Webhook error:", error.message);
+        // Only send error response if no response has been sent yet
+        return res.status(400).json({ success: false, message: "Webhooks error" });
     }
 }
